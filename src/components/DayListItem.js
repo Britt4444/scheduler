90ef817a3f58403e1c0451769eaa5ff846838495
Brittany Cowper <brittferren@gmail.com>
2023-06-14 12:11:52 -0500

    style DayListItem with conditional classes

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,8 +1,18 @@
 import React from "react";
+import 'components/DayListItem.scss';
+import classNames from "classnames";
 
 export default function DayListItem(props) {
+
+  let dayListItemClass = classNames(
+    'day-list__item', {
+    "--selected": props.selected,
+    "--full": !props.spots
+ });
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li
+    className={dayListItemClass}
+    onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{props.spots} spots remaining</h3>
     </li>
