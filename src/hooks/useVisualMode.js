7ef34a17126afbe2eb5a1c7812eb75423436470d
Brittany Cowper <brittferren@gmail.com>
2023-06-26 23:57:50 -0500

    build custom hook useVisualMode

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false) {
+    if (mode !== newMode) {
+      setMode(newMode);
+    }
+    (replace ? setHistory(history) : history.push(newMode));
+  }
+
+  function back() {
+    if (history.length === 1) {
+      return;
+    }
+    history.pop();
+    setHistory(history);
+    setMode(history[history.length - 1]);
+  }
+
+  return { mode, transition, back };
+
+};
